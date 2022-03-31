2c5263e766fe3fe57862bbe022c2ef260bfe57c7
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-03-31 20:04:41 -0300

    add to env

--- nodeJS-youtube/modulesJS/express.js
+++ nodeJS-youtube/modulesJS/express.js
@@ -9,7 +9,7 @@ const port = 3000
 
 app.get('/users', async (req, res) => {
   try {
-    const users = await UserModel.find({})
+    const users = await UserModel.find({firstName: 'Marcelo'})
     res.status(200).json(users)
   } catch (error) {
     res.status(500).json({ error: error.message })
