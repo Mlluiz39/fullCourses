93cd7ffc485d48493d374528cb8f958ce47c3771
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-05-18 18:45:12 -0300

    add to menu id

--- nlw-return/main.js
+++ nlw-return/main.js
@@ -1,7 +1,22 @@
 const navigation = document.querySelector('[data-js="navigation"]')
+const body = document.querySelector('body')
+const openNavigation = document.querySelector('[data-js="open-navigation"]')
+const closeNavigation = document.querySelector('[data-js="close-navigation"]')
+
+const home = document.querySelector('[data-js="home"]')
+const services = document.querySelector('[data-js="services"]')
+const about = document.querySelector('[data-js="about"]')
 
 document.addEventListener('scroll', () => {
   scrollY > 0
     ? navigation.classList.add('scroll')
     : navigation.classList.remove('scroll')
+})
+
+openNavigation.addEventListener('click', () => {
+  body.classList.add('menu-expanded')
+})
+
+closeNavigation.addEventListener('click', () => {
+  body.classList.remove('menu-expanded')
 })
